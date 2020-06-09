a41b2e54587dd0856d348c53d46b933a559f41c4
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-08 18:48:48 -0700

    Tokyo object updated and hourly sales recorded to site

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 // ==================Object Literal - Seattle===========================
-
 var seattleLocation = {
   location : 'Seattle',
   minCustomer : 23,
   maxCustomer : 65,
   avgCookie : 6.3,
   cookieSales : [],
+  totalDailyArry : [],
   calculateSales : function(){
     for(var i = 0; i < openHours.length; i++){
 
@@ -17,20 +17,35 @@ var seattleLocation = {
 
       var cookiesPerHr = Math.round(possibleCustomers * this.avgCookie);
 
-      var hourlyOutput = (openHours[i] + ': ' + cookiesPerHr);
+      var hourlyOutput = (openHours[i] + ': ' + cookiesPerHr + ' cookies');
       this.cookieSales.push(hourlyOutput);
+      this.totalDailyArry.push(cookiesPerHr);
     };
-  }
+  },
+  // totalDailySales : function(){
+  //   var total = 0
+  //   dailyNum = parseInt(this.totalDailyArry[i]);
+  //   for(var i=0; i<openHours.length; i++){
+  //     total = total + dailyNum;
+  //   }
+  //   this.totalDailyArry.push(total);
+  // },
 };
 
-seattleLocation.calculateSales();
+seattleLocation.calculateSales();  //Calling Seattle Function
+// seattleLocation.totalDailySales();
 
-console.log(seattleLocation.cookieSales);
+// console.log(seattleLocation.cookieSales);
+console.log('Total Daily Array:', seattleLocation.totalDailyArry);
+// console.log('Total Daily Sales:', seattleLocation.totalDailySales());
+
 var seattleUnorderedList = document.getElementById('seattle');
-var newListItem = document.createElement('li');
-newListItem.textContent = seattleLocation.cookieSales[0];
-seattleUnorderedList.appendChild(newListItem);
 
+for (var i = 0; i<openHours.length; i++){
+  var newListItem = document.createElement('li');
+  newListItem.textContent = seattleLocation.cookieSales[i];
+  seattleUnorderedList.appendChild(newListItem);
+};
 
 // ==================Object Literal - Tokyo===========================
 
@@ -38,8 +53,33 @@ var tokyoLocation = {
   location : 'Tokyo',
   minCustomer : 3,
   maxCustomer : 24,
-  avgCookie : 1.2
+  avgCookie : 1.2,
+  cookieSales : [],
+  totalDailyArry : [],
+  calculateSales : function(){
+    for(var i = 0; i < openHours.length; i++){
+
+      var possibleCustomers = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer + 1)) + this.minCustomer;
+
+      var cookiesPerHr = Math.round(possibleCustomers * this.avgCookie);
+
+      var hourlyOutput = (openHours[i] + ': ' + cookiesPerHr + ' cookies');
+      this.cookieSales.push(hourlyOutput);
+      this.totalDailyArry.push(cookiesPerHr);
+    };
+  },
 };
+
+tokyoLocation.calculateSales();
+
+var tokyoUnorderedList = document.getElementById('tokyo');
+
+for (var i = 0; i<openHours.length; i++){
+  var newListItem = document.createElement('li');
+  newListItem.textContent = tokyoLocation.cookieSales[i];
+  tokyoUnorderedList.appendChild(newListItem);
+};
+
 
 // ==================Object Literal - Dubai===========================
 
