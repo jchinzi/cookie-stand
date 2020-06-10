b19031cf886f32e11e769183d1b5730252357f94
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-09 18:14:10 -0700

    Fixed Formatting of time in Row 1, Added Dubai data to table

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'strict use';
 
-var openHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var openHours = ['6:00am', '7:00am', '8:00am', '9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm', '6:00pm', '7:00pm'];
 
 // ====================================================================
 // =========================Function List==============================
@@ -298,7 +298,30 @@ function renderCookiesToTableTokyo(){
   //=======================End Row 1 Cells / Append Row 1 to Table==========================
   table.appendChild(tableRowTokyo);
 }
+//================================Begin Row 4====================================
+
+function renderCookiesToTableDubai(){
+  var table = document.getElementById('cookieChart');
+  var tableRowDubai = document.createElement('tr');
+  // ==============================First Cell - City Name ================================
+  var tableCell = document.createElement('td');
+  tableCell.textContent = dubaiLocation.location;
+  tableRowDubai.appendChild(tableCell);
+  // ========End City Cell / Begin Loop for Cookies Sold on Rest of Row 2=================
+  for (var i = 0; i<openHours.length; i++){
+    tableCell = document.createElement('td');
+    tableCell.textContent = dubaiLocation.cookieSales[i];
+    tableRowDubai.appendChild(tableCell);
+  }
+  tableCell = document.createElement('td');
+  tableCell.textContent = dubaiLocation.calculateDailySum();
+  tableRowDubai.appendChild(tableCell);
+  //=======================End Row 1 Cells / Append Row 1 to Table==========================
+  table.appendChild(tableRowDubai);
+}
 
 renderCookiesToTableRow1();
 renderCookiesToTableSeattle();
 renderCookiesToTableTokyo();
+renderCookiesToTableDubai();
+
