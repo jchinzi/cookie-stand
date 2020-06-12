9aab130d75d5eb3f6101d7a04121562903d7bbc7
Jennifer Chinzi <jenrose87@gmail.com>
2020-06-11 19:24:26 -0700

    Form can now add new location row to chart but does NOT update total yet

--- js/app.js
+++ js/app.js
@@ -11,16 +11,90 @@ var cookieStoreArray = [];
 // Will store the total cookies sold each hour
 var allSalesHourly = [];
 
+// var allSalesArray = [];
+
+// var updatedTotalSalesArray = [];
+
 // =========================================================================================
 // ==============================Function List==============================================
 // =========================================================================================
 
+// Function to Delete Final Row=============================================================
+// https://stackoverflow.com/questions/10686888/delete-last-row-in-table
+
+function deleteFinalRow(tableID){
+  var table = document.getElementById(tableID);
+  var rowCount = table.rows.length;
+
+  table.deleteRow(rowCount -1);
+}
+
+// Function to Interact with Form on sales.html===========================================
+
+var newStoreForm = document.getElementById('franchise');
+
+newStoreForm.addEventListener('submit', addUserStoreToTable);
+
+function addUserStoreToTable(event){
+  event.preventDefault();
+
+  deleteFinalRow('cookieChart');  //Delete Original Totals Row
+
+  var location = event.target.location.value;
+  var minCustomer = event.target.minCustomer.value;
+  var maxCustomer = event.target.maxCustomer.value;
+  var avgCookie = event.target.avgCookie.value;
+
+  console.log(location, minCustomer, maxCustomer, avgCookie);
+  var newStore = new CookieShop(location, minCustomer, maxCustomer, avgCookie);
+
+  // newStore.addNewStoreToArray();
+  newStore.calculateHourlyCookies();
+  newStore.calculateCustomersHourly();
+  newStore.calculateDailySum();
+  newStore.renderStoreDataToTable();
+  // newStore.pushSalesArray();
+
+  renderCookiesToTableRowFinal();  //ReRender Totals Row
+
+  console.log('New Store', newStore);
+  // console.log(allSalesArray);
+}
+
 // Function to Add New Stores to cookieStoreArray===========================================
 
-function addNewStoreToArray(){
-  var newLocation = this.location;
-  cookieStoreArray.push(newLocation);
-}
+// function addNewStoreToArray(){
+//   var newLocation = this.location;
+//   cookieStoreArray.push(newLocation);
+// }
+
+// Function to Add Hourly Sales Array to allSalesArray===========================================
+
+//  function pushSalesArray (){
+//  var storeSalesArray = this.cookieSales;
+//  allSalesArray.push(storeSalesArray);
+// }
+
+// Function to Fill updatedTotalSalesArray===========================================
+
+// function updateTotalSales(){
+
+//   var totalCookies = 0;
+//   for (var hrIndex = 0; hrIndex < openHours.length; hrIndex++){
+
+//     for (var storeIndex = 0; storeIndex < cookieStoreArray.length; cookieStoreArray++){
+
+//       totalCookies += allSalesArray[hrIndex][storeIndex];
+//       console.log('Total Cookies1', totalCookies);
+//     }
+//     console.log('Total Cookies2', totalCookies);
+//     // updatedTotalSalesArray.push(totalCookies);
+//   }
+//   console.log('Total Cookies3',totalCookies);
+//   // console.log('Updated Total Sales Array', updatedTotalSalesArray);
+//   console.log('All Sales Array', allSalesArray);
+// }
+// updateTotalSales();
 
 // Randomizer Function:  adapted from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 
@@ -154,10 +228,37 @@ function renderCookiesToTableRowFinal(){
   //=======================End Row 4 Cells / Append Row 4 to Table==========================
   table.appendChild(tableRowFinal);
 }
+// =========================================================================================
+//================================Alternative Final Row with User Input=====================
+
+// function renderAllCookiesToFinalRow(){
+//   var table = document.getElementById('cookieChart');
+//   var tableRowFinal = document.createElement('tr');
+//   var tableCell = document.createElement('td');
+//   tableCell.textContent = 'Totals';
+//   tableRowFinal.appendChild(tableCell);
+
+//   var allCookiesSold =
+
+//   for (var i=0; i<openHours.length; i++){
+
+//     tableCell=document.createElement('td');
+//     tableCell.textContent = ;
+//     tableRowFinal.appendChild(tableCell);
+//   }
+//   tableCell = document.createElement('td');
+//   tableCell.textContent = '';
+//   tableRowFinal.appendChild(tableCell);
+//   table.appendChild(tableRowFinal);
+// }
+
+
 
 // ====================================================================
 // ==================Constructor Function==============================
 // ====================================================================
+
+var allStores = [];
 
 function CookieShop(location, minCustomer, maxCustomer, avgCookie, unorderedListID){
   this.location = location;
@@ -166,6 +267,7 @@ function CookieShop(location, minCustomer, maxCustomer, avgCookie, unorderedList
   this.avgCookie = avgCookie;
   this.unorderedListID = unorderedListID;
   this.cookieSales = [];
+  allStores.push(this);
 }
 
 CookieShop.prototype.calculateCustomersHourly = calculateCustomersHourly;
@@ -173,7 +275,8 @@ CookieShop.prototype.calculateHourlyCookies = calculateHourlyCookies;
 CookieShop.prototype.calculateDailySum = calculateDailySum;
 CookieShop.prototype.renderStore = renderStore;
 CookieShop.prototype.renderStoreDataToTable = renderStoreDataToTable;
-CookieShop.prototype.addNewStoreToArray = addNewStoreToArray;
+// CookieShop.prototype.addNewStoreToArray = addNewStoreToArray;
+// CookieShop.prototype.pushSalesArray = pushSalesArray;
 
 // ===========ReCreate Objects with Constructor Function=================
 
@@ -186,68 +289,92 @@ var limaLocation = new CookieShop('Lima',2,16,4.6,'lima');
 
 // Call Seattle Functions
 
-seattleLocation.addNewStoreToArray();
+// seattleLocation.addNewStoreToArray();
 seattleLocation.calculateHourlyCookies();
 seattleLocation.calculateCustomersHourly();
 seattleLocation.calculateDailySum();
+// seattleLocation.pushSalesArray();
 // seattleLocation.renderStore();
 // console.log(seattleLocation.cookieSales);
 
 
 // Call Tokyo Function
 
-tokyoLocation.addNewStoreToArray();
+// tokyoLocation.addNewStoreToArray();
 tokyoLocation.calculateHourlyCookies();
 tokyoLocation.calculateCustomersHourly();
 tokyoLocation.calculateDailySum();
+// tokyoLocation.pushSalesArray();
 // tokyoLocation.renderStore();
 // console.log(tokyoLocation.cookieSales);
 
 
 //Call Dubai Function
 
-dubaiLocation.addNewStoreToArray();
+// dubaiLocation.addNewStoreToArray();
 dubaiLocation.calculateHourlyCookies();
 dubaiLocation.calculateCustomersHourly();
 dubaiLocation.calculateDailySum();
+// dubaiLocation.pushSalesArray();
 // dubaiLocation.renderStore();
 // console.log(dubaiLocation.cookieSales);
 
 
 //Call Paris Function
 
-parisLocation.addNewStoreToArray();
+// parisLocation.addNewStoreToArray();
 parisLocation.calculateHourlyCookies();
 parisLocation.calculateCustomersHourly();
 parisLocation.calculateDailySum();
+// parisLocation.pushSalesArray();
 // parisLocation.renderStore();
 // console.log(parisLocation.cookieSales);
 
 
 // Call Lima Functions
 
-limaLocation.addNewStoreToArray();
+// limaLocation.addNewStoreToArray();
 limaLocation.calculateHourlyCookies();
 limaLocation.calculateCustomersHourly();
 limaLocation.calculateDailySum();
+// limaLocation.pushSalesArray();
 // limaLocation.renderStore();
 // console.log(limaLocation.cookieSales);
 
 // =============================================================================================
 //========Function to Sum Cookies by Hour and fill All Sales Hourly Array=======================
 
+// TODO: Newly added stores are not being added to the totals line
+
 function sumAllTimes(){
-  for (var i = 0; i < openHours.length; i++){
-    var salesByTime = seattleLocation.cookieSales[i]+
-    tokyoLocation.cookieSales[i]+
-    dubaiLocation.cookieSales[i]+
-    parisLocation.cookieSales[i]+
-    limaLocation.cookieSales[i];
+  for (var hrIndex = 0; hrIndex < openHours.length; hrIndex++){
+    var salesByTime = 0;
+    for (var storeIndex = 0; storeIndex < allStores.length; storeIndex++){
+      salesByTime += allStores[storeIndex].cookieSales[hrIndex];
+    }
     allSalesHourly.push(salesByTime);
   }
 }
 sumAllTimes();
-// console.log('Sales Array Test:', allSalesHourly);
+console.log('Hourly Sales Array Test:', allSalesHourly);
+
+// function sumAllTimesAllStores(){
+//   for (var hrIndex = 0; hrIndex < openHours.length; hrIndex++){
+//     for (var cityIndex = 0; cityIndex <cookieStoreArray.length; cityIndex++){
+
+      
+//     }
+//       var salesByTime = 
+//       allSalesArray[0][hrIndex]+
+//       allSalesArray[1][hrIndex]+
+//       allSalesArray[2][hrIndex]+
+//       allSalesArray[3][hrIndex]+
+//       allSalesArray[4][hrIndex];
+//       updatedTotalSalesArray.push(salesByTime);
+//     }
+// }
+// sumAllTimesAllStores();
+// console.log('Total Sales Array', updatedTotalSalesArray);
 
 // =============================================================================================
 //===================================Call Table Render Functions================================
